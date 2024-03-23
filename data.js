f2a4754c578345247893ee3bf00eaee9b72bec32
Rajesh Kumar Mahato <96366694+rajesh44411@users.noreply.github.com>
2024-03-23 11:06:54 +0000

    javascricpt

---
+++ data.js
@@ -0,0 +1,17 @@
+let name = "rajesh kumar mahato"
+let email = "singh@gmail.com"
+const id = 423
+/*
+1. number 2 to power 53
+bigint 
+boolean
+string
+undefind
+null
+
+
+object = {firstName: "Rajesh", secondName: "Kumar"}
+symbol
+
+
+*/
---
+++ var.js
@@ -0,0 +1,7 @@
+const accountId = 5421
+var accountName = "raj@email.com"
+let accountCity = "Janakpur"
+
+// always use let var is outdated to create variable
+
+console.table([accountCity, accountId, accountName])
