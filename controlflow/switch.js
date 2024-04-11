2b63858cbdabd3a59a7b067e49518f928eea7385
Rajesh Kumar Mahato <96366694+rajesh44411@users.noreply.github.com>
2024-04-11 11:21:23 +0000

    switch

---
+++ controlflow/switch.js
@@ -0,0 +1,20 @@
+const Months = "Feb"
+
+
+switch (Months) {
+    case "Janaury":
+        console.log("jan");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        break;
+}
