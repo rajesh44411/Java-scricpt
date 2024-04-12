1c5ddb7853ad01a7b0a38cab05c52a0e514c8dbc
Rajesh Kumar Mahato <96366694+rajesh44411@users.noreply.github.com>
2024-04-12 17:49:46 +0000

    while do loop

---
+++ controlflow/whiledo.js
@@ -0,0 +1,20 @@
+// let i = 0
+// while (i <= 40) {
+//     // console.log(`value of  ${i}`); 
+//     i++
+// }
+
+// let myArr = [ "ram" , "shyam" ,"hari"]
+
+// let arr = 0
+
+// while (arr < myArr.length) {
+//     // console.log(myArr[arr]);
+//     // arr++  or  arr=+2
+// }
+
+ let score = 10
+do {
+    console.log(`value of score is ${score}`);
+    score++
+} while (score <= 20);
